efaffd118fbbb3e4e6ba0cd169391cefc0edebcf
Grzegorz Piszczek <piszczek@tkomp.pl>
2024-10-07 19:06:30 +0200

    db structure ver2

--- DB_Structure.sql
+++ DB_Structure.sql
@@ -1,60 +1,87 @@
+/*
 CREATE DATABASE employee_schedule
 GO
+*/
 -----------Grafiki pracownicze i ewidencja czasu pracy----------
 -----------created by:Grzegorz Piszczek-------------------------
-CREATE TABLE dbo.employee_schedule_type
+USE [employee_schedule]
+GO
+BEGIN TRANSACTION
+BEGIN TRY
+
+CREATE TABLE [dbo].[employee_schedule_type]
 (
-ID_employee_schedule_type INT NOT NULL IDENTITY(1,1)
+[ID_employee_schedule_type] INT NOT NULL IDENTITY(1,1)
 ,[name] VARCHAR(30) NOT NULL
-,avilable BIT NOT NULL DEFAULT = 1
+,[avilable] BIT NOT NULL DEFAULT 1
 )
-GO
-INSERT INTO employee_schedule_type ([name]) VALUES ('Planowany');
-INSERT INTO employee_schedule_type ([name]) VALUES ('Wykonania');
-GO
-CREATE TABLE employee_absenteeism
+
+INSERT INTO [dbo].[employee_schedule_type] ([name]) VALUES ('Planowany')
+INSERT INTO [dbo].[employee_schedule_type] ([name]) VALUES ('Wykonania')
+
+CREATE TABLE [dbo].[employee_absenteeism]
 (
-ID_employee_absenteeism INT NOT NULL IDENTITY(1,1)
+[ID_employee_absenteeism] INT NOT NULL IDENTITY(1,1)
 ,[short_name] varchar(2) NOT NULL
 ,[name] VARCHAR(30) NOT NULL
-,avilable BIT NOT NULL DEFAULT = 1
+,[avilable] BIT NOT NULL DEFAULT 1
 )
-GO
-INSERT INTO employee_absenteeism ([short_name],[name]) VALUES ('R','Dzienna zmiana');
-INSERT INTO employee_absenteeism ([short_name],[name]) VALUES ('N','Nocna zmiana');
-INSERT INTO employee_absenteeism ([short_name],[name]) VALUES ('L4','Chorobowe');
-INSERT INTO employee_absenteeism ([short_name],[name]) VALUES ('UW','Urlop wypoczynkowy');
-INSERT INTO employee_absenteeism ([short_name],[name]) VALUES ('UZ','Urlop na ¿¹danie');
-GO
-CREATE TABLE dbo.employee
+
+INSERT INTO [dbo].[employee_absenteeism] ([short_name],[name]) VALUES ('R','Dzienna zmiana')
+INSERT INTO [dbo].[employee_absenteeism] ([short_name],[name]) VALUES ('N','Nocna zmiana')
+INSERT INTO [dbo].[employee_absenteeism] ([short_name],[name]) VALUES ('L4','Chorobowe')
+INSERT INTO [dbo].[employee_absenteeism] ([short_name],[name]) VALUES ('UW','Urlop wypoczynkowy')
+INSERT INTO [dbo].[employee_absenteeism] ([short_name],[name]) VALUES ('UZ','Urlop na ¿¹danie')
+
+CREATE TABLE [dbo].[employee]
 (
-ID_employee INT NOT NULL IDENTITY(1,1)
-,employee_name VARCHAR(30) NOT NULL
-,avilable BIT NOT NULL DEFAULT = 1
+[ID_employee] INT NOT NULL IDENTITY(1,1)
+,[employee_name] VARCHAR(30) NOT NULL
+,[avilable] BIT NOT NULL DEFAULT 1
 )
-GO
-CREATE TABLE dbo.employee_schedule
+ALTER TABLE [dbo].[employee]
+	ADD CONSTRAINT PK_employee
+	PRIMARY KEY ([ID_employee])
+
+CREATE TABLE [dbo].[employee_schedule]
 (
-ID_employee_schedule INT NOT NULL IDENTITY(1,1)
+[ID_employee_schedule] INT NOT NULL IDENTITY(1,1)
 ,[year] INT not null
 ,[month] INT not null
 ,[name_of_employee_group] VARCHAR(30) NOT NULL
-,[avilable] BIT NOT NULL DEFAULT = 1
+,[avilable] BIT NOT NULL DEFAULT 1
 )
-GO
-CREATE TABLE dbo.employee_schedule_position
+ALTER TABLE [dbo].[employee_schedule]
+	ADD CONSTRAINT PK_employee_schedule
+	PRIMARY KEY ([ID_employee_schedule])
+
+ALTER TABLE [dbo].[employee_schedule]
+	ADD CONSTRAINT UQ_employee_schedule
+	UNIQUE ([year],[month],[avilable])
+
+CREATE TABLE [dbo].[employee_schedule_position]
 (
 [ID_employee_schedule_position] INT NOT NULL IDENTITY(1,1)
-,[employee_schedule_ID] INT NOT NULL --FK as named constraint
-,[employee_ID] INT NOT NULL --FK as named constraint TODO
+,[employee_schedule_ID] INT NOT NULL	--FK
+,[employee_ID] INT NOT NULL				--FK
 ,[for_date] DATETIME NOT NULL 
 ,[employee_absenteeism_ID] INT NOT NULL
 ,[hours] DATETIME NOT NULL
 )
-GO
-ALTER TABLE employee_schedule_position
+
+ALTER TABLE [dbo].[employee_schedule_position]
 	ADD CONSTRAINT FK_employee_schedule_employee_schedule_position
-	FOREIGN KEY (employee_schedule_ID) REFERENCES employee_schedule(ID_employee_schedule) ON DELETE CASCADE; 
-GO
+	FOREIGN KEY ([employee_schedule_ID]) REFERENCES [dbo].[employee_schedule]([ID_employee_schedule]) ON DELETE CASCADE
+
+ALTER TABLE [dbo].[employee_schedule_position]
+	ADD CONSTRAINT FK_employee_employee_schedule_position
+	FOREIGN KEY ([employee_ID]) REFERENCES [dbo].[employee]([ID_employee]) ON DELETE CASCADE
+
+COMMIT
+END TRY
+BEGIN CATCH
+	ROLLBACK
+	SELECT 'ERROR OCCURRED'
+END CATCH
 
 -----------------------------------------------
